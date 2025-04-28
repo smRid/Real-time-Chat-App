13ff6ca515b8b4fecef240ba9ca7cd8df939acc0
smRid <omorsk911@gmail.com>
2025-04-28 20:28:57 +0600

    profile auth, cloudinary, auth routes added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import  cloudinary  from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -83,4 +84,37 @@ export const logout = (req, res) => {
       console.log("Error in logout controller", error.message);
       res.status(500).json({ message: "Internal Server Error" });
     }
-  };
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+      const { profilePic } = req.body;
+      const userId = req.user._id;
+  
+      if (!profilePic) {
+        return res.status(400).json({ message: "Profile pic is required" });
+      }
+  
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { profilePic: uploadResponse.secure_url },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.log("error in update profile:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const checkAuth = (req, res) => {
+    try {
+      res.status(200).json(req.user);
+    } catch (error) {
+      console.log("Error in checkAuth controller", error.message);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { login, logout, signup,  } from "../controllers/auth.controller.js";
-
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
