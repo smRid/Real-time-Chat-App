93e887804884609b98dbc0b5a8245fa6a7d1708f
smRid <omorsk911@gmail.com>
2025-04-28 20:57:15 +0600

    message model, message routes, messageSchema added

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from "./routes/message.route.js";
 
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port:' + PORT);
