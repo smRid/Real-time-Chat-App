5aaa85876b5c4206f25ae24be5fe604dcc1e1937
smRid <omorsk911@gmail.com>
2025-05-01 03:59:27 +0600

    fullstack build

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import path from "path";
+
 import { connectDB } from './lib/db.js';
 
 import authRoutes from './routes/auth.route.js';
@@ -13,6 +15,7 @@ import { app, server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +26,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log('Server is running on port:' + PORT);
