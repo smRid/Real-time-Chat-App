b940d2967216f93df533c5d825c0c0106235c8c1
smRid <omorsk911@gmail.com>
2025-05-01 02:06:32 +0600

    Chat Scrollview added

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,5 @@
 import { useChatStore } from "../store/useChatStore"
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
@@ -16,13 +16,23 @@ const ChatContainer = () => {
     subscribeToMessages,
     unsubscribeFromMessages,} = useChatStore(); 
   const { authUser }=useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
+
     subscribeToMessages();
 
     return () => unsubscribeFromMessages();
-  },[selectedUser._id,getMessages, subscribeToMessages, unsubscribeFromMessages])
+  },[selectedUser._id,getMessages, subscribeToMessages, unsubscribeFromMessages]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({behavior: "smooth"});
+    } 
+  },[messages]);
+    
+    
 
   if (isMessagesLoading) {
     return (
@@ -44,7 +54,8 @@ const ChatContainer = () => {
         {messages.map((message) => (
           <div
             key={message._id}
-            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}>
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
               <img
