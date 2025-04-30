4781d21671f10528119bbc6a0e0b5d11103e0589
smRid <omorsk911@gmail.com>
2025-05-01 02:29:49 +0600

    Seed users added, ChatContainer, Sidebar updated

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useChatStore } from "../store/useChatStore"
+import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
@@ -9,13 +9,14 @@ import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
   const {
-    messages, 
+    messages,
     getMessages,
     isMessagesLoading,
     selectedUser,
     subscribeToMessages,
-    unsubscribeFromMessages,} = useChatStore(); 
-  const { authUser }=useAuthStore();
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
 
   useEffect(() => {
@@ -24,15 +25,13 @@ const ChatContainer = () => {
     subscribeToMessages();
 
     return () => unsubscribeFromMessages();
-  },[selectedUser._id,getMessages, subscribeToMessages, unsubscribeFromMessages]);
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
   useEffect(() => {
     if (messageEndRef.current && messages) {
-      messageEndRef.current.scrollIntoView({behavior: "smooth"});
-    } 
-  },[messages]);
-    
-    
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   if (isMessagesLoading) {
     return (
@@ -43,34 +42,33 @@ const ChatContainer = () => {
       </div>
     );
   }
-    
-
 
   return (
-    <div className="flex-1 flex-col overflow-auto">
-      <ChatHeader/>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
           <div
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-            ref={messageEndRef}>
-            <div className="chat-image avatar">
+            ref={messageEndRef}
+          >
+            <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
-              <img
-              src={
-              message.senderId === authUser._id
-              ? authUser.profilePic || "/avatar.png"
-              : selectedUser.profilePic || "/avatar.png"
-              }
-              alt="profile pic"
-              />
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
               </div>
-              </div>
-              <div className="chat-header mb-1">
+            </div>
+            <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1">
-              {formatMessageTime(message.createdAt)}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
             <div className="chat-bubble flex flex-col">
@@ -81,18 +79,14 @@ const ChatContainer = () => {
                   className="sm:max-w-[200px] rounded-md mb-2"
                 />
               )}
-              {message.text && 
-                <p>{message.text}</p>
-              }
+              {message.text && <p>{message.text}</p>}
             </div>
-            </div>
-      ))}
+          </div>
+        ))}
       </div>
-      <MessageInput/>
-      
-    </div>
-    
-  )
-}
 
-export default ChatContainer
+      <MessageInput />
+    </div>
+  );
+};
+export default ChatContainer;
