102edb60e2aa4b3ba931524e7811a02a816e58ad
smRid <omorsk911@gmail.com>
2025-04-30 12:18:01 +0600

    MessagesLoading ui update

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore"
+import { useEffect } from "react";
+
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
+  const {messages, getMessages,isMessagesLoading,selectedUser,} = useChatStore(); 
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  },[selectedUser._id,getMessages])
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  };
+    
+
+
   return (
-    <div>
+    <div className="flex-1 flex-col overflow-auto">
+      <ChatHeader/>
+
+      <p>messages...</p>
+
+      <MessageInput/>
       
     </div>
+    
   )
 }
 
