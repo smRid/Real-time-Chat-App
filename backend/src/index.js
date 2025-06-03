f5a2e9a2b1ab46dfac330157da086e58210ae99f
smRid <omorsk911@gmail.com>
2025-06-04 02:46:24 +0600

    add cron job for periodic GET requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from './lib/socket.js';
 
+import job from './lib/cron.js'
+
 
 dotenv.config();
 
@@ -23,6 +25,8 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+
+if (process.env.NODE_ENV === 'production') job.start();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
